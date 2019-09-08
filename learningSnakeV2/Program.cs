ef905e2afec327a86ac8d7d67e5cce38812e67af
YanaiH <yhyh0203@gmail.com>
2019-09-08 11:48:15 +0300

    Trying to put everything into a solution, deleting the rest after commit.

---
+++ learningSnakeV2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experiments
+{
+    class experiments
+    {
+        public static void input()
+        {
+            int xpos = Console.WindowWidth / 2, ypos = Console.WindowHeight / 2;
+            Console.SetCursorPosition(xpos, ypos);
+            while (true)
+            {
+                ConsoleKey keyInput = Console.ReadKey().Key;
+
+                switch (keyInput)
+                {
+                    case ConsoleKey.RightArrow:
+                        Console.Write(">");
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        Console.Write("<");
+                        Console.SetCursorPosition(xpos - 1, ypos);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        Console.Write("^");
+                        Console.SetCursorPosition(xpos, ypos - 1);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.Write("v");
+                        Console.SetCursorPosition(xpos, ypos + 1);
+                        break;
+                    case ConsoleKey.C:
+                        Console.Clear();
+                        Console.SetCursorPosition(xpos, ypos);
+                        break;
+                    default: break;
+
+                }
+
+                xpos = Console.CursorLeft;
+                ypos = Console.CursorTop;
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            experiments.input();
+
+        }
+
+    }
+}
